9fec7147ee044a4b1613b87dcf33798122b4509b
jkawashiri <jkawashiri5@gmail.com>
2023-08-07 10:22:50 -0700

    defines PLAYERS constants and state variables and sets up initialize and render functions

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 /*----- constants -----*/
-
+const PLAYERS = {
+    '1': 'Player',
+    '-1': 'Dealer',
+}
 
 /*----- state variables -----*/
-
+let dealerTotal, playerTotal, cards, turn, winner
 
 /*----- cached elements  -----*/
 
@@ -11,3 +14,31 @@
 
 
 /*----- functions -----*/
+init ()
+
+function init() {
+    cards = []
+    dealerTotal = 0
+    playerTotal = 0
+    turn = 1
+    winner = null
+    render()
+}
+
+function render() {
+    renderCards()
+    renderTotals()
+    renderMessage()
+}
+
+function renderCards() {
+
+}
+
+function renderTotals() {
+
+}
+
+function renderMessage() {
+    
+}

e0c6361752bbbe5e9bbf80bed0f3ee5dad69136f
jkawashiri <jkawashiri5@gmail.com>
2023-08-07 12:15:08 -0700

    builds original deck and shuffled deck functions

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ const PLAYERS = {
 
 const suits = ['s', 'c', 'd', 'h']
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A']
+const originalDeck = buildDeck()
 
 /*----- state variables -----*/
 let dealerTotal, playerTotal, cards, turn, winner
@@ -44,4 +45,27 @@ function renderTotals() {
 
 function renderMessage() {
 
+}
+
+function buildDeck() {
+    const deck = []
+    suits.forEach(function(suit) {
+        ranks.forEach(function(rank) {
+            deck.push({
+                face: `${suit}${rank}`,
+                value: Number(rank) || (rank === 'A' ? 11 : 10)
+            })
+        })
+    })
+    return deck
+}
+
+function getShuffledDeck() {
+    const copyDeck = [...originalDeck]
+    const shuffledDeck = []
+    while(copyDeck.length) {
+        const rndIdx = Math.floor(Math.random() * copyDeck.length)
+        shuffledDeck.push(copyDeck.splice(rndIdx, 1)[0])
+    }
+    return shuffledDeck
 }
